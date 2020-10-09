5bc536e45f3018b91fc5fcaf1cb3060c838168ff
MiXaiLL76 <m.stepanov@redmadrobot.com>
2020-10-09 18:09:53 +0300

    Забыл добавить в шоткаты

--- install_latest_node.sh
+++ install_latest_node.sh
@@ -11,3 +11,7 @@ mkdir -p /usr/local/lib/nodejs
 tar -xvf node.tar.gz -C /usr/local/lib/nodejs 
 
 mv /usr/local/lib/nodejs/node-${NODE_LATEST_VERSION}-linux-x64 /usr/local/lib/nodejs/node-latest-linux-x64
+
+ln -s /usr/local/lib/nodejs/node-latest-linux-x64/bin/node /usr/local/bin/node
+ln -s /usr/local/lib/nodejs/node-latest-linux-x64/bin/npm /usr/local/bin/npm
+ln -s /usr/local/lib/nodejs/node-latest-linux-x64/bin/npx /usr/local/bin/npx
